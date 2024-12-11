c80dd86d4c408b1f471315c8964eed77dde06120
MahbuburSQADelineate <mahbubur@delineate.pro>
2024-12-11 18:49:20 +0600

    update

--- newman_executor.js
+++ newman_executor.js
@@ -2,23 +2,28 @@ const newman = require("newman");
 
 newman.run(
   {
-    collection: require("./Test-Suite.postman_collection.json"),
-    environment: require("./Staging_Env.postman_environment.json"),
-
+    collection: require("./collections/Test-Suite.postman_collection.json"), // Path to your collection
+    environment: require("./collections/Staging_Env.postman_environment.json"), // Path to your environment
     iterationCount: 1,
     reporters: "htmlextra",
     reporter: {
       htmlextra: {
-        export: "./Reports/report.html",
-        title: "Delineate API Test",
-        browserTitle: "Delineate QA"
+        export: "./Reports/report.html", // Path to save the HTML report
+        title: "API Test Report",
+        browserTitle: "Test Results",
       },
     },
+    globalVar: [
+      {
+        key: "filePath",
+        value: "./collections/main.pdf", // Path to the file for upload
+      },
+    ],
   },
   function (err) {
     if (err) {
       throw err;
     }
-    console.log("collection run complete!");
+    console.log("Collection run complete!");
   }
 );
